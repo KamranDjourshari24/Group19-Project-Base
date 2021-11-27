a609ce76c686562d526fb69d7f079cedfd8222cb
Faith Wilkins El <44947772+lavendermistyco@users.noreply.github.com>
2021-11-27 12:06:20 -0500

    Not sure how to use owl carousel and our images from the data base

--- public/top-100/top-100Script.js
+++ public/top-100/top-100Script.js
@@ -91,7 +91,6 @@ document.addEventListener('DOMContentLoaded', () => {
   imageExtractor(top100List);
 
   const movieList = ['Avengers:+Endgame', 'The+Dark+Knight', 'Django+Unchained', 'Spider-Man:+Into+the+Spider-Verse'];
-  let movieCounter = 1;
   async function getImage(titleList) {
     await Promise.all(titleList.map(async (title) => {
       const movieVal = encodeURIComponent(title.trim());
@@ -101,20 +100,111 @@ document.addEventListener('DOMContentLoaded', () => {
       const imageRequest = await fetch(`${apiImageLink}${backdropPath}`);
       const img = await imageRequest.blob();
       const imgSource = URL.createObjectURL(img);
-      const slideShow = `#slide${movieCounter}`;
-      const slide = document.getElementById(slideShow);
-      slide.src = imgSource;
-      slide.alt = title;
-      movieCounter += 1;
     }));
   }
   getImage(movieList);
   let counter = 1;
   setInterval(() => {
-    document.getElementById(`radio${counter}`).checked = true;
     counter += 1;
     if (counter > 4) {
       counter = 1;
     }
   }, 5000);
+});
+
+$(document).ready(function () {
+  const bigimage = $("#big");
+  let thumbs = $("#thumbs");
+  let syncedSecondary = true;
+
+  bigimage
+  .owlCarousel({
+    items: 1,
+    slideSpeed: 2000,
+    nav: true,
+    animateOut: "fadeOut",
+    autoplay: true,
+    dots: false,
+    loop: true,
+    responsiveRefreshRate: 200,
+    navText: [
+      '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+      '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+    ],
+  })
+  .on("changed.owl.carousel", syncPosition);
+
+  thumbs
+    .on("initialized.owl.carousel", function () {
+      thumbs.find(".owl-item").eq(0).addClass("current");
+    })
+    .owlCarousel({
+      items: 7,
+      dots: true,
+      nav: true,
+      navText: [
+        '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
+        '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
+      ],
+      smartSpeed: 200,
+      slideSpeed: 500,
+      slideBy: 4,
+      responsiveRefreshRate: 100,
+      responsive:{
+        0:{
+            items:2,
+        },
+        600:{
+            items:5,
+        },
+        1000:{
+          items:7,
+      }
+    },
+    })
+    .on("changed.owl.carousel", syncPosition2);
+  function syncPosition(el) {
+      //if loop is set to false, then you have to uncomment the next line
+      //var current = el.item.index;
+  
+      //to disable loop, comment this block
+      var count = el.item.count - 1;
+      var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+      // $("#counter").html("item " + current + " of " + count);
+      if (current < 0) {
+        current = count;
+      }
+      if (current > count) {
+        current = 0;
+      }
+      //to this
+      thumbs
+        .find(".owl-item")
+        .removeClass("current")
+        .eq(current)
+        .addClass("current");
+      var onscreen = thumbs.find(".owl-item.active").length - 1;
+      var start = thumbs.find(".owl-item.active").first().index();
+      var end = thumbs.find(".owl-item.active").last().index();
+  
+      if (current > end) {
+        thumbs.data("owl.carousel").to(current, 100, true);
+      }
+      if (current < start) {
+        thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+      }
+    }
+  
+    function syncPosition2(el) {
+      if (syncedSecondary) {
+        var number = el.item.index;
+        bigimage.data("owl.carousel").to(number, 100, true);
+      }
+    }
+  
+  thumbs.on("click", ".owl-item", function (e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data('owl.carousel').to(number, 300, true);
+  });
 });
